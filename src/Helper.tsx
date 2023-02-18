f59c1540ae61f30b3ee9ed6cca59670dd7bcd0bc
gwyn-art <gwyn.artstyle@gmail.com>
2023-02-19 01:25:42 +0200

    v1.1 Improved by copilot

--- src/Helper.tsx
+++ src/Helper.tsx
@@ -7,7 +7,8 @@ export const Helper = () => {
             User arrow keys to navigate the player. <br/>
             The goal is to push all the boxes (B) to the targets (T). <br/>
             Press "z" to undo your last move.
-            Press "n" to go to the next level.
+            Press "n" to go to the next level. <br/>
+            Press "r" to restart the level.
         </p>
     )
 }

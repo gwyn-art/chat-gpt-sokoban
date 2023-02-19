78ae0ac47a9d9c138d9fad36a6bfdb94ae10b4bb
gwyn-art <gwyn.artstyle@gmail.com>
2023-02-19 17:14:19 +0200

    add swipe mention to helper component

--- src/Helper.tsx
+++ src/Helper.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Helper = () => {
     return (
         <p>
-            User arrow keys to navigate the player. <br/>
+            User arrow keys or swipe on game field to navigate the player. <br/>
             The goal is to push all the boxes (B) to the targets (T). <br/>
             Press "z" to undo your last move.
             Press "n" to go to the next level. <br/>

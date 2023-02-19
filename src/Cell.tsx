4e9761e1fbd0f9705275c987398fcd2ecb51a142
gwyn-art <gwyn.artstyle@gmail.com>
2023-02-19 18:53:00 +0200

    add icons to helper guide

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -8,6 +8,122 @@ type CellProps = {
   onClick?: () => void;
 };
 
+export const WallIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60">
+    <defs>
+      <pattern
+        id="brick"
+        x="0"
+        y="0"
+        width="30"
+        height="30"
+        patternUnits="userSpaceOnUse"
+      >
+        <rect fill="#A52A2A" x="0" y="0" width="30" height="30" />
+        <rect fill="#8B0000" x="0" y="0" width="30" height="5" />
+        <rect fill="#8B0000" x="0" y="0" width="5" height="30" />
+        <rect fill="#8B0000" x="25" y="0" width="5" height="30" />
+        <rect fill="#8B0000" x="0" y="25" width="30" height="5" />
+      </pattern>
+    </defs>
+    <rect fill="url(#brick)" x="0" y="0" width="60" height="60" />
+  </svg>
+);
+
+export const PlayerIcon = (
+  (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+      <defs>
+        <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
+          <stop
+            offset="0%"
+            style={{ stopColor: "#14b8a6", stopOpacity: 1 }}
+          />
+          <stop
+            offset="100%"
+            style={{ stopColor: "#1dc684", stopOpacity: 1 }}
+          />
+        </linearGradient>
+      </defs>
+      <circle
+        cx="50"
+        cy="50"
+        r="45"
+        fill="url(#grad)"
+        stroke="#222"
+        strokeWidth="4"
+      />
+      <rect
+        x="30"
+        y="25"
+        width="40"
+        height="30"
+        fill="#fff"
+        stroke="#222"
+        strokeWidth="4"
+      />
+      <circle cx="45" cy="45" r="10" fill="#222" />
+      <circle cx="55" cy="45" r="10" fill="#222" />
+      <path
+        d="M 40 65 Q 50 75 60 65"
+        stroke="#222"
+        strokeWidth="4"
+        fill="none"
+      />
+    </svg>
+  )
+)
+
+export const BoxIcon = (
+  (
+    <svg viewBox="0 0 100 100">
+      <rect
+        width="100"
+        height="100"
+        fill="#ababab"
+        stroke="#333333"
+        strokeWidth="2"
+      />
+      <rect
+        x="10"
+        y="10"
+        width="80"
+        height="80"
+        fill="#dddddd"
+        stroke="#333333"
+        strokeWidth="2"
+      />
+      <rect
+        x="20"
+        y="20"
+        width="60"
+        height="60"
+        fill="#ffffff"
+        stroke="#333333"
+        strokeWidth="2"
+      />
+    </svg>
+  )
+)
+
+export const GoalIcon = (
+  (
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="10"
+        y="10"
+        width="80"
+        height="60"
+        fill="#FFD700"
+        stroke="#000"
+        stroke-width="2"
+      />
+      <line x1="20" y1="50" x2="80" y2="50" stroke="#000" stroke-width="2" />
+      <circle cx="50" cy="50" r="5" fill="#000" />
+    </svg>
+  )
+)
+
 export const Cell: React.FC<CellProps> = ({
   isWall,
   isPlayer,
@@ -20,121 +136,19 @@ export const Cell: React.FC<CellProps> = ({
 
   if (isWall) {
     backgroundColor = "black";
-    content = (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60">
-        <defs>
-          <pattern
-            id="brick"
-            x="0"
-            y="0"
-            width="30"
-            height="30"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect fill="#A52A2A" x="0" y="0" width="30" height="30" />
-            <rect fill="#8B0000" x="0" y="0" width="30" height="5" />
-            <rect fill="#8B0000" x="0" y="0" width="5" height="30" />
-            <rect fill="#8B0000" x="25" y="0" width="5" height="30" />
-            <rect fill="#8B0000" x="0" y="25" width="30" height="5" />
-          </pattern>
-        </defs>
-        <rect fill="url(#brick)" x="0" y="0" width="60" height="60" />
-      </svg>
-    );
+    content = WallIcon;
   } else if (isPlayer) {
     backgroundColor = "white";
-    content = (
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <defs>
-          <linearGradient id="grad" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop
-              offset="0%"
-              style={{ stopColor: "#14b8a6", stopOpacity: 1 }}
-            />
-            <stop
-              offset="100%"
-              style={{ stopColor: "#1dc684", stopOpacity: 1 }}
-            />
-          </linearGradient>
-        </defs>
-        <circle
-          cx="50"
-          cy="50"
-          r="45"
-          fill="url(#grad)"
-          stroke="#222"
-          strokeWidth="4"
-        />
-        <rect
-          x="30"
-          y="25"
-          width="40"
-          height="30"
-          fill="#fff"
-          stroke="#222"
-          strokeWidth="4"
-        />
-        <circle cx="45" cy="45" r="10" fill="#222" />
-        <circle cx="55" cy="45" r="10" fill="#222" />
-        <path
-          d="M 40 65 Q 50 75 60 65"
-          stroke="#222"
-          strokeWidth="4"
-          fill="none"
-        />
-      </svg>
-    );
+    content = PlayerIcon;
     if (isGoal) {
       backgroundColor = "green";
     }
   } else if (isBox) {
     // backgroundColor = "brown";
-    content = (
-      <svg viewBox="0 0 100 100">
-        <rect
-          width="100"
-          height="100"
-          fill="#ababab"
-          stroke="#333333"
-          strokeWidth="2"
-        />
-        <rect
-          x="10"
-          y="10"
-          width="80"
-          height="80"
-          fill="#dddddd"
-          stroke="#333333"
-          strokeWidth="2"
-        />
-        <rect
-          x="20"
-          y="20"
-          width="60"
-          height="60"
-          fill="#ffffff"
-          stroke="#333333"
-          strokeWidth="2"
-        />
-      </svg>
-    );
+    content = BoxIcon;
   } else if (isGoal) {
     backgroundColor = "green";
-    content = (
-      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-        <rect
-          x="10"
-          y="10"
-          width="80"
-          height="60"
-          fill="#FFD700"
-          stroke="#000"
-          stroke-width="2"
-        />
-        <line x1="20" y1="50" x2="80" y2="50" stroke="#000" stroke-width="2" />
-        <circle cx="50" cy="50" r="5" fill="#000" />
-      </svg>
-    );
+    content = GoalIcon;
   } else {
     backgroundColor = "white";
     content = (
--- src/Helper.tsx
+++ src/Helper.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React from "react";
+import { BoxIcon, GoalIcon, PlayerIcon } from "./Cell";
 
+const HelperIcon: React.FC<{ children: JSX.Element }> = ({ children }) => (
+  <div style={{ display: "inline-block", width: "15px", height: "15px" }}>
+    {children}
+  </div>
+);
 
 export const Helper = () => {
-    return (
-        <p>
-            User arrow keys or swipe on game field to navigate the player. <br/>
-            The goal is to push all the boxes (B) to the targets (T). <br/>
-            Press "z" to undo your last move.
-            Press "n" to go to the next level. <br/>
-            Press "r" to restart the level.
-        </p>
-    )
-}
+  return (
+    <p>
+      User arrow keys or swipe on game field to navigate the player (
+      <HelperIcon>{PlayerIcon}</HelperIcon>). <br />
+      The goal is to push all the boxes (<HelperIcon>{BoxIcon}</HelperIcon>) to
+      the goal (<HelperIcon>{GoalIcon}</HelperIcon>). <br />
+      Press "z" to undo your last move. Press "n" to go to the next level.{" "}
+      <br />
+      Press "r" to restart the level.
+    </p>
+  );
+};
